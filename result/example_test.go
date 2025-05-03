ef59788d38a2133c739b4b80f3da1f6cbcf9e7e5
Christian Schulz <mail@chris-s.eu>
2025-05-03 13:04:00 +0200

    result: Adds example for Must and String

--- result/example_test.go
+++ result/example_test.go
@@ -37,6 +37,40 @@ func ExampleOf() {
 	// Got error: final token
 }
 
+func ExampleResult_Must() {
+	resultSuccess := result.Of(getValue(1))
+
+	fmt.Println(resultSuccess.Must())
+
+	func() {
+		defer func() {
+			v := recover()
+			if v != nil {
+				fmt.Println("Panicked:", v)
+			}
+		}()
+
+		resultError := result.Of(getError(io.EOF))
+		fmt.Println(resultError.Must())
+	}()
+
+	// Output:
+	// 1
+	// Panicked: Must called on result with error: [*errors.errorString]: EOF
+}
+
+func ExampleResult_String() {
+	resultSuccess := result.Of(getValue(1))
+	resultError := result.Of(getError(io.EOF))
+
+	fmt.Println(resultSuccess.String())
+	fmt.Println(resultError.String())
+
+	// Output:
+	// (Result[int]: 1)
+	// (Result[int]: <error[*errors.errorString]: EOF>)
+}
+
 func getValue(i int) (int, error) {
 	return i, nil
 }
